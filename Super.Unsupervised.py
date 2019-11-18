f30c57751f5f9c8ac8240ec031d67ebd1093a750
dana6691 <dahee.kim@mnsu.edu>
2019-11-18 16:24:35 -0600

    kmean/knn
    Signed-off-by: dana6691 <dahee.kim@mnsu.edu>

---
+++ Super.Unsupervised.py
@@ -0,0 +1,177 @@
+from sklearn import datasets
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+iris = datasets.load_iris()
+type(iris)
+
+print(iris.keys())
+
+print(type(iris.data), type(iris.target))
+print(iris.data.shape)
+
+#EDA
+X=iris.data
+y=iris.target
+df = pd.DataFrame(X,columns=iris.feature_names)
+print(df.head())
+print(df.describe())
+
+#bar graph(Yes/No)
+plt.figure()
+sns.countplot(x='education', hue='party', data=df, palette='RdBu')
+plt.xticks([0,1], ['No', 'Yes'])
+plt.show()
+
+#fit and predict the model
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=6)
+knn.fit(iris['data'],iris['target']) #fit
+print(iris['data'].shape)
+print(iris['target'].shape)
+
+prediction = knn.predict(X_new)
+X_new.shape
+print('Prediction {}'.format(prediction))
+################################################
+#fit/predict kkn
+################################################
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X,y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
+################################################
+#measuring performance
+################################################
+from sklearn.model_selection import train_test_split
+X_train, X_test,y_train,y_text = train_test_split(X,y,test_size=0.3,random_state=21, stratify=y)
+
+################################################
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+####################################################
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=7)
+
+# Fit the classifier to the training data
+knn.fit(X_train,y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+###########################################
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+#################################################
+#K-means clustering
+#################################################
+# Import KMeans
+from sklearn.cluster import KMeans
+
+# Create a KMeans instance with 3 clusters: model
+model = KMeans(n_clusters=3)
+
+# Fit model to points
+model.fit(points)
+
+# Determine the cluster labels of new_points: labels
+labels =model.predict(new_points)
+
+# Print cluster labels of new_points
+print(labels)
+#################################################
+#scatter plot
+import matplotlib.pyplot as plt 
+xs = samples[:,0]
+ys = samples[:,2]
+plt.scatter(xs,ys,c=labels)
+plt.show()
+###################################################
+# Import pyplot
+import matplotlib.pyplot as plt
+
+# Assign the columns of new_points: xs and ys
+xs = new_points[:,0]
+ys = new_points[:,1]
+
+# Make a scatter plot of xs and ys, using labels to define the colors
+plt.scatter(xs,ys,c=labels,alpha=0.5)
+
+# Assign the cluster centers: centroids
+centroids = model.cluster_centers_
+
+# Assign the columns of centroids: centroids_x, centroids_y
+centroids_x = centroids[:,0]
+centroids_y = centroids[:,1]
+
+# Make a scatter plot of centroids_x and centroids_y
+plt.scatter(centroids_x,centroids_y,marker='D',s=50)
+plt.show()
+
+###################################################
