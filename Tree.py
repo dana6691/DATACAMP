a78451bdc7a557cd709bbb8edbe4ed2f00faa741
dana6691 <dahee.kim@mnsu.edu>
2019-12-03 11:57:17 -0600

    final
    Signed-off-by: dana6691 <dahee.kim@mnsu.edu>

--- Tree.py
+++ Tree.py
@@ -1,10 +1,99 @@
+#####################################################
+#Supervised Learning
+#####################################################
+#Decision Regions: region in the feature space, (negative/positive decision by the decision boundary)
+#Classification And Regression Tree(CART)
+    #sequence of if-else about individual features
+    #Goal: infer class labels
+    #Able to capture non-linear relationship between features and labels
+    #Don't require feature scaling(ex)standardization)
+        #ex)Cancer(Y/N)
+#####################################################
+#Classification tree
+import pandas as pd
+df2 = pd.read_csv("C:/Users/daheekim/Desktop/datacamp_data/breast_cancer.csv")
+#print(df.head())
+y=df2['diagnosis']
+#print(list(df2.columns) )
+X=df2['radius_mean','concave points_mean']#??????????????
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+X_train,X_test, y_train,y_test = train_test_split(X,y,test_size=0.2,stratify=y, random_state=1)
+df=DecisionTreeClassifier(max_depth=2, random_state=1)
+df.fit(X_train,y_train)
+y_pred=df.predict(X_test) # Predict test set labels
+print(y_pred[0:5])
+
+from sklearn.metrics import accuracy_score
+y_pred = dt.predict(X_test)# Predict test set labels
+acc = accuracy_score(y_test, y_pred)# Compute test set accuracy
+print("Test set accuracy: {:.2f}".format(acc))
+#####################################################
+#Logistic Regreession VS Classification Tree
+from sklearn.linear_model import  LogisticRegression
+logreg = LogisticRegression(random_state=1)
+logreg.fit(X_train, y_train)# Fit
+clfs = [logreg, dt]# Define a list called clfs containing the two classifiers logreg and dt
+plot_labeled_decision_regions(X_test, y_test, clfs)# Plot decision regions of the two classifiers
+#####################################################
+#Decision tree for Classification
+    #node: Root(no parent node,2 children nodes)/Internal Node(1 parent node,2 children nodes)/Leaf()
+        #The existence of a node depends on the state of its predecessors.
+    #criteria to measure the impurity of a node:
+        #1)gini index
+        #2)entropy
+    #at each node, split the data based on feature and split-point to maximize information gain(IG)
+#####################################################
+#Entropy 
+from sklearn.tree import DecisionTreeClassifier
+dt_entropy = DecisionTreeClassifier(max_depth=8,  criterion='entropy', random_state=1) #'entropy' as the information criterion
+dt_entropy.fit(X_train, y_train)# Fit dt_entropy to the training set
+#Gini index
+from sklearn.tree import DecisionTreeClassifier
+dt_gini = DecisionTreeClassifier(max_depth=8,  criterion='gini', random_state=1) #'gini' as the information criterion
+dt_gini.fit(X_train, y_train)# Fit dt_gini to the training set
+#Entropy vs Gini index
+from sklearn.metrics import accuracy_score
+y_pred= dt_entropy.predict(X_test) # Use dt_entropy
+accuracy_entropy = accuracy_score(y_test, y_pred)# Evaluate accuracy_entropy
+y_pred= dt_gini.predict(X_test) # Use dt_gini
+accuracy_gini = accuracy_score(y_test, y_pred)# Evaluate accuracy_gini
+print('Accuracy achieved by using entropy: ', accuracy_entropy)# Print accuracy_entropy
+print('Accuracy achieved by using the gini index: ', accuracy_gini)# Print accuracy_gini
+#####################################################
+#Decision tree for Classification
+#####################################################
+#Regression tree
+from sklearn.tree import DecisionTreeRegressor
+dt = DecisionTreeRegressor(max_depth=8,
+             min_samples_leaf=0.13,
+            random_state=3)
+dt.fit(X_train, y_train)
+from sklearn.metrics import mean_squared_error as MSE
+y_pred = dt.predict(X_test)# Compute y_pred
+mse_dt = MSE(y_test, y_pred)# Compute mse_dt
+rmse_dt = mse_dt**(1/2)# Compute rmse_dt
+print("Test set RMSE of dt: {:.2f}".format(rmse_dt))
+#Linear regression
+y_pred_lr = lr.predict(X_test)# Predict test 
+mse_lr = MSE (y_pred_lr, y_test)# Compute mse_lr
+rmse_lr = mse_lr**(1/2)# Compute rmse_lr
+print('Linear Regression test set RMSE: {:.2f}'.format(rmse_lr))
+print('Regression Tree test set RMSE: {:.2f}'.format(rmse_dt))
 ################################################
-#Supervised machine learning
-#Generalization Error = bias^2 + variance + error
-    #bias(accurate=low bias): how much f and f^ different
-    #variance(precise=low variance): how much f^ is inconsistent over different training sets
-    #model complexity: increase --> variance increase, bias decreases
-        #goal: find the lowest generalization error
+#Supervised Learning
+    #Fit the model f(x) that best approximates(f(x) can be logistic regression, decision tree, neural network)
+    #discard noise as much as possible
+    #low predictive error on unseen dataset
+#difficulties
+    #overfitting: predictive power is low
+    #underfitting: training set error = test set error ,but errors are high
+#Generalization Error = bias^2 + variance + irreducible error
+    '''*bias(accurate=low bias): how much f and f^ different,High bias = Underfitting
+       *variance(precise=low variance): how much f^ is inconsistent over different training sets, High variance = Overfitting
+       *model complexity: flexibility of f^ (increase --> variance increase, bias decreases) ---> Bias-variance Tradeoff
+          #goal: find the lowest model complexity, generalization error'''
 ################################################
 ################################################
 #Diagnosis bias and variance = estimating Generalization Error
@@ -13,59 +102,107 @@
         3) evaluate error of f^ on test set
         4) generalization error of f^ = test set error of f^'''
 #Better model evaluation with Cross-Validation
-    #K-fold CV: CV error = (E1+E2+E3..E10)/10
+    #K-fold CV: CV error = (E1+E2+E3..E10)/10, training fold=9, validation fold=1
     #Hold-out CV
 #If f^ high variance: CV error of f^ > training error of f^
     #Remedy overfitting
         '''1)decrease model complexity
            2)gather mode data'''
-#if f^ high bias: CV error of f^ = training error of f^ >> desirede error
+#if f^ high bias: CV error of f^ = training error of f^ >> desired error
     #Remedy underfitting
         '''1)increase model complexity
            2)gather more relevant features'''
 
 ################################################
-# Import train_test_split from sklearn.model_selection
 from sklearn.model_selection import train_test_split
-
-# Set SEED for reproducibility
 SEED = 1
-
-# Split the data into 70% train and 30% test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=SEED)
-
-# Instantiate a DecisionTreeRegressor dt
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=SEED)# Split the data into 70% train and 30% test
 dt = DecisionTreeRegressor(max_depth=4, min_samples_leaf=0.26, random_state=SEED)
-
-
-################################################
-# Compute the array containing the 10-folds CV MSEs
-MSE_CV_scores = - cross_val_score(dt, X_train, y_train, cv=10, 
-                                  scoring='neg_mean_squared_error', 
-                                  n_jobs=-1) 
-
-# Compute the 10-folds CV RMSE
-RMSE_CV = (MSE_CV_scores.mean())**(1/2)
-
-# Print RMSE_CV
+#Evaluate 10-fold CV error
+MSE_CV_scores = - cross_val_score(dt, X_train, y_train, cv=10, scoring='neg_mean_squared_error', n_jobs=-1) # 10-folds CV MSEs
+RMSE_CV = (MSE_CV_scores.mean())**(1/2)# Compute the 10-folds CV RMSE
 print('CV RMSE: {:.2f}'.format(RMSE_CV))
-################################################
 #Evaluate the training error
-################################################
 from sklearn.metrics import mean_squared_error as MSE 
-dt.fit(X_train, y_train)# Fit dt to the training set
-y_pred_train = dt.predict(X_train)# Predict the labels of the training set
+dt.fit(X_train, y_train)
+y_pred_train = dt.predict(X_train)# Predict
 RMSE_train = (MSE(y_train, y_pred_train))**(1/2)# Evaluate the training set RMSE of dt
-print('Train RMSE: {:.2f}'.format(RMSE_train))# Print RMSE_train
+print('Train RMSE: {:.2f}'.format(RMSE_train))
 ################################################
+#CARTs(Advan)
+    #simpled to understand/interpret
+    #easy to use
+    #flexibility: ability to describe non-linear dependecies
+    #no need to standardize or normalization features
+#CARTs(DisAdvan)
+    #Classification: only produce orthogonal decision boundaries
+    #Sensitive to small variations in training set
+    #High variance: unconstrained CARTs may overfit training set
+        #Solution: Ensembal learning
 #Ensemble learning
+    #train different models on the same dataset
+    #each model makes predictions
+    #Meta-model: aggregates predictions of individual model
+    #Final prediction: more robust and less prone to errors
+#Ensemble Final prediction
+    #Voting Classifier
+        '''1)Hard Voting: binary classification(0/1)
+           #Same training set
+           #!= algorithm'''
+    #Bagging(Bootstrap Aggregation) Classifier
+        '''#one algorithm
+           #Different subsets of training set
+               #reduce variane of individual models in the ensemble
+            #Classification: majority voting
+            #Regression: averaing '''
+        #problem: some instances may not be sampled at all
+            #not sampled instances == Out of Bag(OOB)
 ################################################
-
+#Ensemble
+SEED=1
+lr = LogisticRegression(random_state=SEED)
+knn = KNN(n_neighbors=27)
+dt = DecisionTreeClassifier(min_samples_leaf=0.13, random_state=SEED)
+classifiers = [('Logistic Regression', lr), ('K Nearest Neighbours', knn), ('Classification Tree', dt)]# Define the list classifiers
+for clf_name, clf in classifiers:    
+    clf.fit(X_train, y_train)      # Fit clf to the training set  
+    y_pred = clf.predict(X_test)    # Predict y_pred
+    accuracy = accuracy_score(y_test, y_pred)     # Calculate accuracy
+    print('{:s} : {:.3f}'.format(clf_name, accuracy))    # Evaluate clf's accuracy on the test set
+#Voting Classifier 
+from sklearn.ensemble import VotingClassifier 
+vc = VotingClassifier(estimators=classifiers)
+vc.fit(X_train, y_train)   
+y_pred = vc.predict(X_test)# Evaluate the test set predictions
+accuracy = accuracy_score(y_test, y_pred)# Calculate accuracy score
+print('Voting Classifier: {:.3f}'.format(accuracy))
+#Bagging Classifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier 
+dt = DecisionTreeClassifier(random_state=1)
+bc = BaggingClassifier(base_estimator=dt, n_estimators=50, random_state=1)
+bc.fit(X_train, y_train) #fit
+y_pred = bc.predict(X_test)# Predict test set labels
+acc_test = accuracy_score(y_test, y_pred)# Evaluate acc_test
+print('Test set accuracy of bc: {:.2f}'.format(acc_test)) 
+#OOB evaluation
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
+dt = DecisionTreeClassifier(min_samples_leaf=8, random_state=1)
+bc = BaggingClassifier(base_estimator=dt, 
+            n_estimators=50,
+            oob_score=True,
+            random_state=1)
+bc.fit(X_train, y_train)
+y_pred = bc.predict(X_test)# Predict 
+acc_test = accuracy_score(y_test, y_pred)# Evaluate test set accuracy
+acc_oob = bc.oob_score_# Evaluate OOB accuracy
+print('Test set accuracy: {:.3f}, OOB accuracy: {:.3f}'.format(acc_test, acc_oob))# Print acc_test and acc_oob
 ################################################
 #Random Forest
     '''*base estimator: decision tree
         *each estimtaor is trained on a different bootstrap sample having the same size as the training set
-        *further randomninzation in the training of individual tress
+        *further randomization in the training of individual tress
         *'d' features are sampled at each node *without* replacement'''
     '''1)each sample different features
         2)decision tree
@@ -222,18 +359,6 @@ best_model = grid_rf.best_estimator_
 y_pred = best_model.predict(X_test)# Predict test set labels
 rmse_test = MSE(y_test,y_pred)**(1/2)# Compute rmse_test
 print('Test RMSE of best model: {:.3f}'.format(rmse_test)) 
-################################################
-#Evaluate the training error
-################################################
-################################################
-#Evaluate the training error
-################################################
-################################################
-#Evaluate the training error
-################################################
-################################################
-#Evaluate the training error
-################################################
 ################################################
 #Evaluate the training error
 ################################################
