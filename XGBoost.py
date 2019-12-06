b0591f4bcd2e84a4fcefab3ba81322d1240825cc
dana6691 <dahee.kim@mnsu.edu>
2019-12-06 15:27:47 -0600

    Update
    Signed-off-by: dana6691 <dahee.kim@mnsu.edu>

--- XGBoost.py
+++ XGBoost.py
@@ -71,6 +71,21 @@ print((cv_results["test-auc-mean"]).iloc[-1])# Print the AUC
     #Computer vision
     #Natural Lanuage preprocessing
     # # of training samples is significantly smaller than features
+##Regression with XGBoost
+    #Metrics: 
+     '''1)RMSE(Root Mean Square Error)
+        2)MAE(Mean Absolute Error)'''
+#Objective(loss) function
+    #Quantifies how far off the predictions from the actual result
+    #Measures the difference between estimated and true values
+    #Goal: find the model that yield the minimum value of the loss function
+        #Regression: 'reg:linear
+        #Classification, just decision:'reg:logistic'
+        #Want probability: 'binary:logistic'
+#Base learners
+    #individual models=base learners
+    #combined to create final prediction that is non-linear
+    #two type: linear, tree
 #################################################
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 xg_reg = xgb.XGBRegressor()
@@ -83,32 +98,98 @@ print("RMSE: %f" % (rmse))
 DM_train = xgb.DMatrix(data=X_train, label=y_train)
 DM_test =  xgb.DMatrix(data=X_test, label=y_test)
 params = {"booster":"gblinear", "objective":"reg:linear"}# Create the parameter dictionary: params
-xg_reg = xgb.train(params = params, dtrain=DM_train, num_boost_round=5# Train the model: xg_reg
+xg_reg = xgb.train(params = params, dtrain=DM_train, num_boost_round=5)# Train the model: xg_reg
 preds = xg_reg.predict(DM_test)
 rmse = np.sqrt(mean_squared_error(y_test,preds)) #RMSE
 print("RMSE: %f" % (rmse))
 
 #Evaluating model quality
-# Create the DMatrix: housing_dmatrix
-housing_dmatrix = xgb.DMatrix(data=X, label=y)
-
-# Create the parameter dictionary: params
-params = {"objective":"reg:linear", "max_depth":4}
-
-# Perform cross-validation: cv_results
-cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics="mae", as_pandas=True, seed=123)
-
-# Print cv_results
+housing_dmatrix = xgb.DMatrix(data=X, label=y)# Create the DMatrix:
+params = {"objective":"reg:linear", "max_depth":4}# Create the parameter dictionary
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=4, num_boost_round=5, metrics='mae', as_pandas=True, seed=123)# Perform cross-validation: cv_results
 print(cv_results)
-
-# Extract and print final boosting round metric
 print((cv_results["test-mae-mean"]).tail(1))
 #################################################
-##Regression with XGBoost
-    #Metrics: 
-     '''1)RMSE(Root Mean Square Error)
-        2)MAE(Mean Absolute Error)'''
+#Regularization in XGBoost
+    #want model to contorl on complexity
+    #want model to both accruate and as simple as possible
+    #regularization parameters
+        '''gamma: minimum loss reduction allowed for a split to occur 
+            alpha: L1 regularization on leaf weights. larger values mean more regularization
+            lambda - L2 regularization on leaf weights'''
+#Base Learner:
+    #Linear: sum of linear term, 
+        #weighted sum of linear model --> rarely used
+    #Tree based learner: decision tree
+        #weighted sume of decision tress(nonlinear)
+        # exclusively used
 #################################################
+# Create the DMatrix: housing_dmatrix
+housing_dmatrix = xgb.DMatrix(data=X, label=y)
+reg_params = [1, 10, 100]
+params = {"objective":"reg:linear","max_depth":3}# Create the initial parameter dictionary for varying l2 strength:
+rmses_l2 = []
+for reg in reg_params:
+    params["lambda"] = reg# Update l2 strength
+    cv_results_rmse = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=2, num_boost_round=5, metrics="rmse", as_pandas=True, seed=123)
+    rmses_l2.append(cv_results_rmse["test-rmse-mean"].tail(1).values[0])  # Append
+print("Best rmse as a function of l2:")
+print(pd.DataFrame(list(zip(reg_params, rmses_l2)), columns=["l2","rmse"]))
+
+#Visualizing individual XGBoost trees
+housing_dmatrix = xgb.DMatrix(data=X, label=y)# Create the DMatrix
+params = {"objective":"reg:linear", "max_depth":2}# Create the parameter dictionary
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)# Train the model
+
+# Plot the first tree
+xgb.plot_tree(xg_reg,num_trees=0)
+plt.show()
+
+# Plot the fifth tree
+xgb.plot_tree(xg_reg,num_trees=4)
+plt.show()
+
+# Plot the last tree sideways
+xgb.plot_tree(xg_reg,num_trees=9,rankdir="LR")
+plt.show()
+
+#Visualizing feature importances:
+housing_dmatrix = xgb.DMatrix(data=X, label=y)# Create the DMatrix
+params = {"objective":"reg:linear","max_depth":4 }# Create the parameter dictionary
+xg_reg = xgb.train(params=params, dtrain=housing_dmatrix, num_boost_round=10)# Train the model:
+xgb.plot_importance(xg_reg)# Plot the feature importances
+plt.show()
 #################################################
-# XGBoost in pipelines
+# Tune XGBoost
 #################################################
+#Un-Tuned model
+import pandas as pd
+import xgboost as xgb
+import numpy as npb
+housing_data=pd.read_csv("ames_housing_trimmed_processed.csv")
+X,y=housing_data[housing_data.colunns.tolist()[:-1]],housing_data[housing_data.columns.tolist()[-1]]
+housing_dmatrix = xgb.DMatrix(data=X,lavel=y)
+untuned_params={"objective":"reg:linear"}
+untuned_cv_results_rmse=xgb.cv(dtrain=housing_dmatrix, params=untuned_params,
+nfold=4, metrics="rmse", as_pandas=True, seed=123)
+#Tuned model
+tuned_params={"objective":"reg:linear",'colsample_bytree':0.3,'learning_rate':0.1,'max_depth':5}
+tuned_cv_results_rmse=xgb.cv(dtrain=housing_dmatrix, params=tuned_params,
+nfold=4, num_boost_round=200,metrics="rmse", as_pandas=True, seed=123)
+
+#Tuning the number of boosting rounds
+housing_dmatrix = xgb.DMatrix(data=X, label=y)# Create the DMatrix
+params = {"objective":"reg:linear", "max_depth":3}# Create the parameter dictionary
+num_rounds = [5, 10, 15]# Create list of number of boosting rounds
+final_rmse_per_round = []# Empty list 
+for curr_num_rounds in num_rounds:
+    cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, num_boost_round=curr_num_rounds, metrics="rmse", as_pandas=True, seed=123)
+    final_rmse_per_round.append(cv_results["test-rmse-mean"].tail().values[-1])
+num_rounds_rmses = list(zip(num_rounds, final_rmse_per_round))
+print(pd.DataFrame(num_rounds_rmses,columns=["num_boosting_rounds","rmse"]))
+
+#Automated boosting round selection 
+housing_dmatrix = xgb.DMatrix(data=X, label=y)# Create your housing DMatrix
+params = {"objective":"reg:linear", "max_depth":4}# Create the parameter dictionary
+cv_results = xgb.cv(dtrain=housing_dmatrix, params=params, nfold=3, num_boost_round=50, metrics="rmse", as_pandas=True, seed=123)
+print(cv_results)
